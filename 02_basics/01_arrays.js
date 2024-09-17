70f25f3faa46c88bb0c9ad04703b3f7e2df07361
raishubham8899 <80665038+raishubham8899@users.noreply.github.com>
2024-09-17 07:37:57 +0000

    arrays and objects practise

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,44 @@
+// array
+
+const myArr = [0, 1, 2, 3, 4, 5]
+const myHeors = ["shaktiman", "naagraj"]
+
+const myArr2 = new Array(1, 2, 3, 4)
+// console.log(myArr[1]);
+
+// Array methods
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+
+// myArr.unshift(9)
+// myArr.shift()
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(3));
+
+// const newArr = myArr.join()  // coverts into string
+
+// console.log(myArr);
+// console.log( newArr);
+
+
+// slice, splice
+
+console.log("A ", myArr);
+
+const myn1 = myArr.slice(1, 3)  
+
+console.log(myn1);
+console.log("B ", myArr);
+
+
+const myn2 = myArr.splice(1, 3)  // manipulates the original array and cut down that number of values
+console.log("C ", myArr);
+console.log(myn2);
+
+
+//shallow copy: A shallow copy of an object is a copy whose properties share same references (like in heap)
+//deep copy: A shallow copy of an object is a copy whose properties share same references (like in stack)
+
