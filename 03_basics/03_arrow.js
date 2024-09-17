03b51ffc005f16a8d52db4b9d365220e95bd28e2
raishubham8899 <80665038+raishubham8899@users.noreply.github.com>
2024-09-17 15:16:10 +0000

    iife done

---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,59 @@
+const user = {
+    username: "hitesh",
+    price: 999,
+
+    welcomeMessage: function() {
+        console.log(`${this.username} , welcome to website`);
+        console.log(this);
+    }
+
+}
+
+
+
+// user.welcomeMessage()
+// user.username = "sam"
+// user.welcomeMessage()
+
+// console.log(this);
+
+// function chai(){
+//     let username = "hitesh"
+//     console.log(this.username);
+// }
+
+// chai()
+
+// const chai = function () {
+//     let username = "hitesh"
+//     console.log(this.username);
+// }
+
+const chai =  () => {
+    let username = "hitesh"
+    console.log(this);
+}
+
+
+// chai()
+
+
+//Difference between arrow function and normal function
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2
+// }
+
+// const addTwo = (num1, num2) =>  num1 + num2    //implicit return
+
+// const addTwo = (num1, num2) => ( num1 + num2 )  // if wrapped in curly braces then return keyword has to be written
+
+const addTwo = (num1, num2) => ({username: "hitesh"})
+
+
+console.log(addTwo(3, 4))
+
+
+// const myArray = [2, 5, 3, 7, 8]
+
+// myArray.forEach()
